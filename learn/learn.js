1c782c2d7f48f8dbc00c5f81feb9a7b58581dd0e
mishqaa <s.marmelodova@yandex.ru>
2020-01-21 08:15:47 +0400

    задание #1

--- learn/learn.js
+++ learn/learn.js
@@ -1,4 +1,26 @@
-'use strict'
-let money = prompt('Ваш бюджет на месяц?'),
-   time = prompt('Введите дату в формате YYYY-MM-DD');
+'use strict';
+let money = +prompt('Ваш бюджет на месяц?', ""),
+   time = prompt('Введите дату в формате YYYY-MM-DD', "");
+
+let appData = {
+   budget: money,
+   expenses: {},
+   optionalExpenses: {},
+   income: [],
+   savings: false,
+   timeData: time
+};
+
+let a1 = prompt('Введите обязательную статью расходов в этом месяце', ""),
+   b1 = +prompt('Во сколько обойдется?', ""),
+   a2 = prompt('Введите обязательную статью расходов в этом месяце', ""),
+   b2 = +prompt('Во сколько обойдется?', "");
+
+appData.expenses[a1] = b1;
+
+appData.expenses[a2] = b2;
+
+console.log(alert(appData.budget / 30));
+
+console.log(appData);
 
